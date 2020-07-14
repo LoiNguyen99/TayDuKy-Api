b14becbba839c2b63ec119a8cde05929e6b5b537
Nguyen Van Loi <Loinguyen.hg.n@gmail.com>
2020-07-14 22:21:21 +0700

    upload image

---
+++ TayDuKy/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TayDuKy.Services;
+
+namespace TayDuKy.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost("images")]
+        public async Task<IActionResult> uploadfile([FromForm] ImageData data)
+        {
+            Stream fs = data.File.OpenReadStream();
+            String result =  await FileServices.getFile(fs, data.Name, data.token);
+
+            var Image = new
+            {
+                imageUrl = result
+            };
+
+            return Created("/",Image);
+        }
+
+
+    }
+
+    public class ImageData {
+        public IFormFile File { get; set; }
+        public String Name { get; set; }
+        public String token { get; set; }
+
+    }
+}
---
+++ TayDuKy/Services/FileServices.cs
@@ -0,0 +1,25 @@
+﻿using Firebase.Auth;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TayDuKy.Services
+{
+    public class FileServices
+    {
+        public static async Task<String> getFile(Stream fileStream, String fileName, String apiKey)
+        {
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(apiKey));
+            var login = await auth.SignInAnonymouslyAsync();
+            var task = new Firebase.Storage.FirebaseStorage("tayduky-d785d.appspot.com", new Firebase.Storage.FirebaseStorageOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(login.FirebaseToken)
+            }).Child("equipments").Child(fileName).PutAsync(fileStream);
+
+            return await task;
+
+        }
+    }
+}
